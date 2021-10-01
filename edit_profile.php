27e2844994695fee60b2e9e8e738cbd828906a51
pilgrim07 <faisalhassan199@gmail.com>
2021-10-01 16:17:12 +0530

    edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['user_email'])){
 else{ ?>
 <html>
 <head>
-	<title>Welcomme to Home</title>
+	<title>Edit Profile</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
