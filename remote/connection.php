cfa11e49742de5394af4b2784546970dd8513faf
incmak <ahmedmuieen@gmail.com>
2021-10-15 13:40:22 +0530

    fixed post images not visible and added remote files

---
+++ remote/connection.php
@@ -0,0 +1,4 @@
+<?php
+ob_start(); //Turns on output buffering 
+$con = mysqli_connect("sql205.byetcluster.com","epiz_29916490","OEYueiwjym","epiz_29916490_social_network") or die("unsunccess");
+?>
